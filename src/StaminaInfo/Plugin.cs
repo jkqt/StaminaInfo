e51f36a0f9c3587e4ba8152e064ac114b70eed04
jkqt <jkqtkr@gmail.com>
2025-06-29 11:29:22 -0400

    Add project files.

---
+++ src/StaminaInfo/Plugin.cs
@@ -0,0 +1,16 @@
+﻿using BepInEx;
+using BepInEx.Logging;
+
+namespace StaminaInfo;
+
+[BepInAutoPlugin]
+public partial class Plugin : BaseUnityPlugin
+{
+    internal static ManualLogSource Log { get; private set; } = null!;
+
+    private void Awake()
+    {
+        Log = Logger;
+        Log.LogInfo($"Plugin {Name} is loaded!");
+    }
+}
